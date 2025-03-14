a2db287da5a90fc1a2ca60830bd9a69d4285f921
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-03-14 17:13:34 -0400

    Update Webhook.php

--- src/Webhook.php
+++ src/Webhook.php
@@ -7,25 +7,20 @@ use RPurinton\Validators\WebhookValidators;
 
 class Webhook
 {
-    public static function post(string $url, array $body)
+    public static function post(string $url, array $body): bool
     {
         WebhookValidators::validateUrl($url);
         WebhookValidators::validateBody($body);
-        $result = '';
-        try {
-            $result = json_decode(HTTPS::request([
-                'url' => $url,
-                'method' => 'POST',
-                'headers' => ["Content-Type: application/json;"],
-                'body' => json_encode($body)
-            ]), true);
-            if (isset($result['retry_after'])) {
-                usleep($result['retry_after']);
-                return self::post($url, $body);
-            }
-        } catch (\Exception $e) {
-            error_log($e->getMessage());
+        $result = json_decode(HTTPS::request([
+            'url' => $url,
+            'method' => 'POST',
+            'headers' => ["Content-Type: application/json;"],
+            'body' => json_encode($body)
+        ]), true);
+        if (isset($result['retry_after'])) {
+            usleep($result['retry_after'] * 1000000);
+            return self::post($url, $body);
         }
-        return $result;
+        return true;
     }
 }
