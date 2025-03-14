7fd0d6181cf3637a191c65d705b58495dff1fd54
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-03-14 16:46:49 -0400

    Update Webhook.php

--- src/Webhook.php
+++ src/Webhook.php
@@ -11,15 +11,20 @@ class Webhook
     {
         WebhookValidators::validateUrl($url);
         WebhookValidators::validateBody($body);
-        $result = json_decode(HTTPS::request([
-            'url' => $url,
-            'method' => 'POST',
-            'headers' => ["Content-Type: application/json;"],
-            'body' => json_encode($body)
-        ]), true);
-        if (isset($result['retry_after'])) {
-            usleep($result['retry_after']);
-            return self::post($url, $body);
+        $result = '';
+        try {
+            $result = json_decode(HTTPS::request([
+                'url' => $url,
+                'method' => 'POST',
+                'headers' => ["Content-Type: application/json;"],
+                'body' => json_encode($body)
+            ]), true);
+            if (isset($result['retry_after'])) {
+                usleep($result['retry_after']);
+                return self::post($url, $body);
+            }
+        } catch (\Exception $e) {
+            $result = $e->getMessage();
         }
         return $result;
     }
